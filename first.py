20b78bb389a5de5c0b790f4eeedff1918ac3f521
Uldis Sturms <uldis.sturms@gmail.com>
2017-05-13 10:05:43 +0100

    first assignment done

---
+++ first.py
@@ -0,0 +1,8 @@
+prompt = '> '
+
+print 'Please enter lastname'
+last_name = raw_input(prompt)
+print 'Please enter firstname'
+first_name = raw_input(prompt)
+
+print "%s %s" % (last_name, first_name)
